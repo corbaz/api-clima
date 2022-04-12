3e8ffa399c1c28fc57cc2be4ae8d87c127cd08fd
Julio Corbaz <julio.corbaz@gmail.com>
2022-04-12 15:03:30 -0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/api-clima/',
+  assetsDir: './assets/',
   plugins: [react()],
   preview: {
     port: 9999,
