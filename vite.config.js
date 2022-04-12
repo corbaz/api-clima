d1ab3f8cc3c6250e2c3fb9c306ed1ed479bf8cff
Julio Corbaz <julio.corbaz@gmail.com>
2022-04-12 12:22:37 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, preview } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: '/assets/',
-  base: '/clima/',
+  base: './',
   build: {
-    outDir: '/docs/'
+    outDir: './docs/'
+  },
+  preview: {
+    port: 9999,
+    host: 'localhost',
+    // https: true,
+    // https: {
+    //   key: fs.readFileSync('./certs/localhost.key'),
+    //   cert: fs.readFileSync('./certs/localhost.crt')
+    // },
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:3000',
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       '^/api': ''
+    //     }
+    //   }
+    // }  
   }
 })
